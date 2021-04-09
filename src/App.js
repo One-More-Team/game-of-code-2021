8c3ae20db13eee589204a3ccebc639397009c152
Tibor Turi <tibor.turi@doclerholding.com>
2021-04-09 21:30:55 +0200

    Test for Krisz

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
   render() {
+    const a = { b: { c: 6 } };
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Welcome to React</h2> {a?.b?.c}
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
