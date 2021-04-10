9616c008d46e8dfcfadcaefecc0b575aab30facd
Tibor Turi <tibor.turi@doclerholding.com>
2021-04-10 18:06:47 +0200

    camera mic selector

--- src/saga/index.js
+++ src/saga/index.js
@@ -5,13 +5,20 @@ import AuthSaga from "./auth-saga";
 import SiteLanguage from "./site-language";
 import AppSaga from "./app-saga";
 import ConnectionSaga from "./connection-saga";
+import DeviceSaga from "./device-saga";
 
 function* initialCall() {
   yield put(initApp());
 }
 
 function* Index() {
-  yield all([...AuthSaga, ...SiteLanguage, ...AppSaga, ...ConnectionSaga]);
+  yield all([
+    ...AuthSaga,
+    ...SiteLanguage,
+    ...AppSaga,
+    ...ConnectionSaga,
+    ...DeviceSaga,
+  ]);
   yield call(initialCall);
 }
 
