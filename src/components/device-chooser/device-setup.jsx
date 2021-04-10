81d6aa6aadfede84fd8cccb8b2b267a4ded2d239
Tibor Turi <tibor.turi@doclerholding.com>
2021-04-10 18:07:03 +0200

    missed files to commit

--- src/components/device-chooser/device-setup.jsx
+++ src/components/device-chooser/device-setup.jsx
@@ -1,17 +1,21 @@
 import React, { useState } from "react";
 import DeviceChooser from "./device-selector";
 
+import commonStyles from "./device-selector.module.scss";
+
 const DeviceSetup = () => {
   const [selectedCamera, setSelectedCamera] = useState(null);
   const [selectedMicrophone, setSelectedMicrophone] = useState(null);
 
   return (
-    <DeviceChooser
-      selectedCamera={selectedCamera}
-      setSelectedCamera={setSelectedCamera}
-      selectedMicrophone={selectedMicrophone}
-      setSelectedMicrophone={setSelectedMicrophone}
-    />
+    <div className={commonStyles.Container}>
+      <DeviceChooser
+        selectedCamera={selectedCamera}
+        setSelectedCamera={setSelectedCamera}
+        selectedMicrophone={selectedMicrophone}
+        setSelectedMicrophone={setSelectedMicrophone}
+      />
+    </div>
   );
 };
 
